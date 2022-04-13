78fb48be67561b5559c3cd54846cd290f3235269
dingding-21 <phj8221@naver.com>
2022-04-13 17:55:18 +0900

    [ADD] Level1: javascript, typescript basics
    Related to: #1

---
+++ 1st-seminar/function.js
@@ -0,0 +1,20 @@
+// function menu(dinner) {
+//   return `오늘 메뉴는 ${dinner}입니다~`;
+// }
+
+const menu = (dinner) => {
+  return `오늘 메뉴는 ${dinner}입니다~`;
+};
+
+const str2 = menu('치킨');
+console.log(str2);
+
+const func = (num) => {
+  return num * num;
+};
+
+const multiple = (func, num) => {
+  console.log(func(num));
+};
+
+multiple(func, 4);
