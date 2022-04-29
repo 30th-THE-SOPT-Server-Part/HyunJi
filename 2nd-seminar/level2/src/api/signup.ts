bf781e480077ec86286d87c7c0b62399182654ce
dingding-21 <phj8221@naver.com>
2022-04-29 22:36:42 +0900

    [CHORE] Level2: Signup statuscode
    Related to: #2

--- 2nd-seminar/level2/src/api/signup.ts
+++ 2nd-seminar/level2/src/api/signup.ts
@@ -3,8 +3,8 @@ import { Request, Response } from 'express';
 const router = async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  return res.status(200).json({
-    status: 200,
+  return res.status(201).json({
+    status: 201,
     message: '유저 생성 성공',
     data: { email, password },
   });
